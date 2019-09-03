ac915c9e6287d8daa0c5b1eb4d46dc3821d5f316
feng-master <554286238@qq.com>
2019-09-03 17:45:43 +0800

    Initial commit

---
+++ src/main/java/com/fengmaster/imgame/ImgameApplication.java
@@ -0,0 +1,13 @@
+package com.fengmaster.imgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengmaster/imgame/ImgameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fengmaster.imgame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImgameApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
